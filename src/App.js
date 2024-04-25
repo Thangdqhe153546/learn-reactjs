8900dab45e8417e1a6c73f4777874ea585370feb
Serena <thangdqhe153546@fpt.edu.vn>
2024-04-25 21:06:05 +0700

    commit 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Thangdqhe153546's Project
         </p>
         <a
           className="App-link"
